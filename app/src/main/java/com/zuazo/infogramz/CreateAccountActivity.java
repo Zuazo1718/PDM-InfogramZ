9f5f299a3579531c5a167839bf9de5178ba688b3
Zuazo1718 <jezuma1718@gmail.com>
2021-09-09 12:09:14 -0400

    7 Se realizo el action bar

--- app/src/main/java/com/zuazo/infogramz/CreateAccountActivity.java
+++ app/src/main/java/com/zuazo/infogramz/CreateAccountActivity.java
@@ -3,6 +3,7 @@ package com.zuazo.infogramz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -10,5 +11,46 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        showToolBar(getString(R.string.title_crear_cuenta), true);
+
+
+    }
+
+
+    public void showToolBar(String titulo, boolean botonSubir){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
