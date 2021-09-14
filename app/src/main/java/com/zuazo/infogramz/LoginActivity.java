f39279f78fe7c5439e08ee47aa3a95ee7d2e012e
Zuazo1718 <jezuma1718@gmail.com>
2021-09-13 22:13:43 -0400

    10 Se realizo Fragments

--- app/src/main/java/com/zuazo/infogramz/LoginActivity.java
+++ app/src/main/java/com/zuazo/infogramz/LoginActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.zuazo.infogramz.view.ContainerActivity;
+import com.zuazo.infogramz.view.CreateAccountActivity;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
@@ -18,4 +21,37 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CreateAccountActivity.class);
         startActivity(intent);
     }
+    public void login(View view){
+        Intent intent = new Intent(this, ContainerActivity.class);
+        startActivity(intent);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
