130b4b1a03dabbe243f4f6d097077e2c8628ed57
alexDy1 <kratos19982571@gmail.com>
2021-09-14 23:27:25 -0400

    Se realizo RecyclerView InfogramZ

--- app/src/main/java/com/zuazo/infogramz/view/fragment/HomeFragment.java
+++ app/src/main/java/com/zuazo/infogramz/view/fragment/HomeFragment.java
@@ -2,13 +2,21 @@ package com.zuazo.infogramz.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.zuazo.infogramz.R;
+import com.zuazo.infogramz.adapter.CardViewAdapter;
+import com.zuazo.infogramz.model.Image;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +69,81 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //ToolBar
+        showToolBar(getString(R.string.title_home_fragment), false, view);
+
+        //recycLearview
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+
+        //layout manager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        // el adapter
+        CardViewAdapter cardViewAdapter = new CardViewAdapter(biudImage(), R.layout.cardview_image, getActivity());
+        recyclerView.setAdapter(cardViewAdapter);
+
+        return view;
     }
+
+    public void showToolBar(String titulo, boolean botonSubir, View view){
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(titulo);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+    //Creamos la lista de imagenes
+    public ArrayList<Image> biudImage() {
+        ArrayList<Image> images = new ArrayList<>();
+
+        images.add(new Image("","Carlos Valdivia", "2 dias", "1 me gusta"));
+        images.add(new Image("","Luis Peres", "3 dias", "5 me gusta"));
+        images.add(new Image("","Fernando Torricos", "6 dias", "4 me gusta"));
+        images.add(new Image("","Maria Flores", "5 dias", "6 me gusta"));
+        images.add(new Image("","Rolando Morales", "3 dias", "2 me gusta"));
+        images.add(new Image("","Willian Jaimes", "4 dias", "5 me gusta"));
+        images.add(new Image("","Roberto Paniagua", "2 dias", "3 me gusta"));
+        images.add(new Image("","Lorena Herrera", "2 dias", "8 me gusta"));
+        images.add(new Image("","Laura Caceres", "6 dias", "3 me gusta"));
+        images.add(new Image("","Marcelo Rojas", "9 dias", "1 me gusta"));
+
+        return images;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
