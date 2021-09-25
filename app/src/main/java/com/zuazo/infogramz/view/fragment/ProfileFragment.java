7835a7a28f9dafefb97ccfb0e2f7f2ad8d05fe32
Zuazo1718 <jezuma1718@gmail.com>
2021-09-24 20:10:41 -0400

    Se realizo CircleImageView y SimpleViewBehavior

--- app/src/main/java/com/zuazo/infogramz/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/zuazo/infogramz/view/fragment/ProfileFragment.java
@@ -2,13 +2,21 @@ package com.zuazo.infogramz.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.zuazo.infogramz.R;
+import com.zuazo.infogramz.adapter.CardViewAdapter;
+import com.zuazo.infogramz.model.Image;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +69,49 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //toolbar
+        showToolBar("", false,view);
+
+        // recyclerview
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_profile);
+
+        //layoutmanager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(linearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //el adapter
+        CardViewAdapter cardViewAdapter = new CardViewAdapter(builImages(), R.layout.cardview_image, getActivity());
+        recyclerView.setAdapter(cardViewAdapter);
+
+        return view;
+    }
+    public void showToolBar(String titulo, boolean botonSubir, View view){
+        Toolbar toolbar = view.findViewById(R.id.toolbar_profile);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(titulo);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+
+    //creamos la lista de imagenes
+    public ArrayList<Image> builImages(){
+        ArrayList<Image> images = new ArrayList<>();
+
+        images.add(new Image("https://media-cdn.tripadvisor.com/media/photo-s/0e/1b/f4/2a/fuerte-de-samaipata.jpg","Carlos Valdivia","2 dias","1 me gusta"));
+        images.add(new Image("https://www.laregion.bo/wp-content/uploads/2019/07/coroico-parapente.jpeg","Luis Peres","3 dias","5 me gusta"));
+        images.add(new Image("https://www.wamanadventures.com/blog/wp-content/uploads/2019/06/La-Laguna-Colorada-Waman-Adventures.jpg","Fernendo Torrico","6 dias","4 me gusta"));
+        images.add(new Image("http://assets.cdnelnuevodiario.com/ckeditor/2018/03/25/bolivia-teleferico-de-la-paz.jpg","MAria Flores","5 dias","6 me gusta"));
+        images.add(new Image("https://i2.wp.com/andorreandoporelmundo.com/wp-content/uploads/2019/02/IMG_5577.jpg?w=1000&ssl=1","Rolando Morales","3 dias","2 me gusta"));
+        images.add(new Image("https://media-cdn.tripadvisor.com/media/photo-s/01/3e/b8/46/puerta-del-sol-tiwanaku.jpg","WWillian Jaimes","4 dias","5 me gusta"));
+        images.add(new Image("https://laicismo.org/wp-content/uploads/2017/02/Monumento-Virgen-de-Socavon-en-Oruro-Bolivia.jpg","Roberto Pañagua","2 dias","3 me gusta"));
+        images.add(new Image("https://serturista.com/wp-content/uploads/2014/01/Castillo-de-la-Glorieta-2.jpg","Lorena Herrera","2 dias","8 me gusta"));
+        images.add(new Image("https://i1.wp.com/elcalderoviajero.com/wp-content/uploads/2018/04/bolivia-cochabamba-incachaca-15.jpg?w=750&ssl=1","Laura Caceres","6 dias","3 me gusta"));
+        images.add(new Image("https://www.lostiempos.com/sites/default/files/styles/noticia_detalle/public/media_imagen/2018/1/13/1-_roma_2-_hector_arnez.jpg?itok=UCQEK8Mi","Marcelo Rojas","9 dias","1 me gusta"));
+
+        return images;
     }
 }
